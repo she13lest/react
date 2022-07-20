6141c0e3b946a63d363871fcf0478cffa9bcb082
Anna <she13lest@gmail.com>
2022-07-20 16:53:43 +0200

    first commit

---
+++ lesson1/task1/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+const rootEl = document.querySelector("#root");
+
+const greetingEl = React.createElement(
+  "div",
+  { className: "greeting" },
+  "Hello, React!"
+);
+
+ReactDOM.render(greetingEl, rootEl);
