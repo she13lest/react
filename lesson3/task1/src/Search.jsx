02c547a3ccda3a4193bc61b20541108ac25c604c
Anna <she13lest@gmail.com>
2022-07-22 16:59:02 +0200

    new

---
+++ lesson3/task1/src/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchField = (props) => {
+  console.log(props);
+  return (
+    <div className="search">
+      <h1 className="search__title">
+        {`Hello, ${props.name}. What to search for you?`}
+      </h1>
+      <div className="search__field">
+        <input type="text" className="search__input" />
+        <button className="search__button">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchField;
