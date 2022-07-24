586d973d30b101cc1a32b4778e081966f3621e8e
Anna <she13lest@gmail.com>
2022-07-24 12:56:54 +0200

    new

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = (props) => {
   return (
     <div className="profile">
       <p className="profile__name">
-        {` ${props.userData.firstName} ${props.userData.lastName}`}
+        {`${props.userData.firstName} ${props.userData.lastName}`}
       </p>
       <p className="profile__birth">{`Was born ${birthDay} in ${props.userData.birthPlace}`}</p>
     </div>
