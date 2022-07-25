7b594c881e35f8f0a99dfcfae7429a24128567ac
Anna <she13lest@gmail.com>
2022-07-25 21:31:12 +0200

    new

--- lesson5/task4/src/Colors.jsx
+++ lesson5/task4/src/Colors.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-class Colors extends React.Component {}
+class Colors extends React.Component {
+  render() {
+    return (
+      <div class="counter">
+        <button data-action="decrease" class="counter__button">
+          -
+        </button>
+        <span class="counter__value">0</span>
+        <button data-action="increase" class="counter__button">
+          +
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Colors;
