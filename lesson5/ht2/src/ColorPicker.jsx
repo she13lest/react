ed99dad205b25da11d4ac9c0fea243e6b3900ae2
Anna <she13lest@gmail.com>
2022-07-26 13:47:25 +0200

    new

--- lesson5/ht2/src/ColorPicker.jsx
+++ lesson5/ht2/src/ColorPicker.jsx
@@ -1,20 +1,46 @@
 import React from "react";
 
 class ColorPicker extends React.Component {
-  component(props) {
+  constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      activeColor: null,
+    };
   }
+
+  setTitle = (title) => {
+    this.setState({
+      activeColor: title,
+    });
+  };
+
+  clearTitle = () => {
+    this.setState({
+      activeColor: null,
+    });
+  };
 
   render() {
     return (
       <div>
-        <div className="picker__title">Red</div>
+        <div className="picker__title">{this.state.activeColor}</div>
         <div>
-          <button className="picker__button picker__button_coral"></button>
-          <button className="picker__button picker__button_aqua"></button>
-          <button className="picker__button picker__button_bisque"></button>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseEnter={() => this.setTitle("Coral")}
+            onMouseLeave={() => this.clearTitle()}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseEnter={() => this.setTitle("Aqua")}
+            onMouseLeave={() => this.clearTitle()}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseEnter={() => this.setTitle("Bisque")}
+            onMouseLeave={() => this.clearTitle()}
+          ></button>
         </div>
       </div>
     );
