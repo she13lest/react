9f9f373e8b1662f29cb3b34d179f9d9551276a0b
Anna <she13lest@gmail.com>
2022-07-28 13:18:12 +0200

    new

---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Message from "./Message";
+
+const text1 = "Hello, world!";
+const text2 = "Another exciting text.";
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: null,
+    };
+  }
+
+  setText = (text) => {
+    this.setState({
+      text,
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <Message text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText(text1)}>
+            text1
+          </button>
+          <button className="btn" onClick={() => this.setText(text2)}>
+            text2
+          </button>
+          <button className="btn" onClick={() => this.setText("")}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
