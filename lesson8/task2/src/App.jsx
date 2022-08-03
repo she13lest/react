a95e7c2b2d60cf636e1672d0a9317da86aaf1b86
Anna <she13lest@gmail.com>
2022-08-03 23:25:55 +0200

    new

---
+++ lesson8/task2/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Clock from "./Clock";
+
+class App extends React.Component {
+  state = {
+    visible: true,
+  };
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="clock" onClick={this.toggle}>
+          Toggle
+        </button>
+        <div>
+          {this.state.visible && (
+            <>
+              <Clock location="New York" offset={-5} />
+              <Clock location="Kyiv" offset={2} />
+              <Clock location="London" offset={0} />
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lesson8/task2/src/Clock.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import moment from "moment";
+
+const getTimeWithOffset = (offset) => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  const date = new Date(
+    currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+  );
+  return moment(date).format("h:mm:ss A");
+};
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: props.location,
+      time: props.offset,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: this.state.time,
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{`${getTimeWithOffset(
+          this.state.time
+        )}`}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
