98cb8bd0055afd0e7896bfa231feb5955d3883da
Anna <she13lest@gmail.com>
2022-08-03 18:15:55 +0200

    new

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -24,7 +24,7 @@ class Life extends React.Component {
 
   componentWillUnmount() {
     console.log(
-      "componentWillUnmount(): cleanup before DOM related to component will be remoted"
+      "componentWillUnmount(): cleanup before DOM related to component will be removed"
     );
   }
 
