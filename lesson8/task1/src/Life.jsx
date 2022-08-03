d1a8fb5246d0ccd2be86cb496a07b65d873ae912
Anna <she13lest@gmail.com>
2022-08-03 18:04:24 +0200

    new

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Life extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor: good place to create state");
+  }
+  componentDidMount() {
+    console.log("componentDidMount: API calls, subscriptions");
+  }
+
+  shouldComponentUpdate() {
+    console.log(
+      "shouldComponentUpdate(nextProps, nextState): decide to render or not to render"
+    );
+  }
+
+  componentDidUpdate() {
+    console.log(
+      "componentDidUpdate(prevProps, prevState): some updates based on new props"
+    );
+  }
+
+  componentWillUnmount() {
+    console.log(
+      "componentWillUnmount(): cleanup before DOM related to component will be remoted"
+    );
+  }
+
+  render() {
+    console.log("return React element to build DOM");
+    return <div className="number">{this.props.number}</div>;
+  }
+}
+
+export default Life;
