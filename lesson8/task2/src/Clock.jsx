867e072854b4f4c119e30556150a2ecd34d7b245
Anna <she13lest@gmail.com>
2022-08-03 23:31:19 +0200

    new

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -28,6 +28,10 @@ class Clock extends React.Component {
     }, 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div className="clock">
