480ea1e9908623af51ae363d751df922fd9bfe88
Anna <she13lest@gmail.com>
2022-08-05 13:43:58 +0200

    new

--- lesson8/ht1/src/ConnectionStatus.jsx
+++ lesson8/ht1/src/ConnectionStatus.jsx
@@ -2,11 +2,32 @@ import React from "react";
 
 class ConnectionStatus extends React.Component {
   state = {
-    user: null,
+    connection: "online",
   };
 
+  componentDidMount() {
+    window.addEventListener("online", this.onOnline);
+    window.addEventListener("offline", this.onOffline);
+  }
+
+  onOnline = () => {
+    this.setState({
+      connection: "online",
+    });
+  };
+  onOffline = () => {
+    this.setState({
+      connection: "offline",
+    });
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("online", this.onOnline);
+    window.removeEventListener("offline", this.onOffline);
+  }
+
   render() {
-    return <div className="status status_offline">Offline</div>;
+    return <div className="status status_offline">{this.state.connection}</div>;
   }
 }
 
