25b7556757063415ed9b2261661cea006c8d2598
Anna <she13lest@gmail.com>
2022-08-12 15:58:15 +0200

    new

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 class Search extends React.Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
     return (
-      <form class="search">
-        <input type="text" class="search__input" />
-        <button class="search__button">Search</button>
+      <form className="search">
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.value}
+          className="search__input"
+        />
+        <button className="search__button">Search</button>
       </form>
     );
   }
