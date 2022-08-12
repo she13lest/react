6b0c31b0586065f0134994f183957054f2c086bb
Anna <she13lest@gmail.com>
2022-08-12 16:02:00 +0200

    new

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -11,7 +11,7 @@ class Search extends React.Component {
 
   search = (e) => {
     e.preventDefault();
-    console.log(this.state.value);
+    alert(`Search text: ${this.state.value}`);
   };
 
   render() {
