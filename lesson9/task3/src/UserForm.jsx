2e6e0be0da573a495d9d2f981dbda44865bdd1d4
Anna <she13lest@gmail.com>
2022-08-14 12:27:30 +0200

    new

--- lesson9/task3/src/UserForm.jsx
+++ lesson9/task3/src/UserForm.jsx
@@ -7,7 +7,7 @@ class UserForm extends React.Component {
       (acc, [name, value]) => ({ ...acc, [name]: value }),
       {}
     );
-    console.log(formData);
+    this.props.onSubmit(formData);
   };
 
   setRef = (node) => {
