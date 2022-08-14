5bae6489b7f8a427fbfcf25095b6d07aa5c57d29
Anna <she13lest@gmail.com>
2022-08-14 19:33:30 +0200

    new

--- lesson9/ht2/src/Game.jsx
+++ lesson9/ht2/src/Game.jsx
@@ -78,7 +78,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = "Winner " + winner;
+      status = "Winner: " + winner;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
