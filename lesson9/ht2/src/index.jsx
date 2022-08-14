bded51b7eda74cde2475f8bda5506d2c30e20986
Anna <she13lest@gmail.com>
2022-08-14 19:30:20 +0200

    new

--- lesson9/ht2/src/index.jsx
+++ lesson9/ht2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import User from "./User";
+import Game from "./Game";
 
 const elementRoot = document.querySelector("#root");
 
-ReactDOM.render(<User userId="Github" />, elementRoot);
+ReactDOM.render(<Game />, elementRoot);
