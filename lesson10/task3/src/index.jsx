fb734c753b71828797061280e093d352053752ca
Anna <she13lest@gmail.com>
2022-08-16 17:04:25 +0200

    new

--- lesson10/task3/src/index.jsx
+++ lesson10/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
+import Calculator from "./Calculator";
 
 const elementRoot = document.querySelector("#root");
 
-ReactDOM.render(<App userId="GitHub" />, elementRoot);
+ReactDOM.render(<Calculator />, elementRoot);
