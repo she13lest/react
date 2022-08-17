1433fa08f25094e3e8e71a8ec8f07419f586b4ad
Anna <she13lest@gmail.com>
2022-08-17 16:32:07 +0200

    new

--- lesson11/task2/src/App.jsx
+++ lesson11/task2/src/App.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+
 import Dialog from "./Dialog";
 
 class App extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
   render() {
     return (
-      <div class="app">
-        <button class="btn">Show dialog</button>
-        <Dialog />
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>
+          Show
+        </button>
+        <Dialog
+          isOpen={this.state.isOpen}
+          onClose={this.hideDialog}
+          title="some title"
+        >
+          <p>Some text</p>
+        </Dialog>
       </div>
     );
   }
--- lesson11/task2/src/Dialog.jsx
+++ lesson11/task2/src/Dialog.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-class Dialog extends React.Component {
- 
-
-  render() {
-    return (
-      <div className="dialog">
-    <div className="dialog__heading">
-      <h4 className="dialog__title">Recommendation</h4>
-      <button className="dialog__close-btn">+</button>
-    </div>
-    <div className="dialog__content">
-      <!-- Dialog children -->
-      <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
-    </div>
-  </div>
-    );
+const Dialog = ({ isOpen, children, title, onClose }) => {
+  if (!isOpen) {
+    return null;
   }
-}
+
+  return (
+    <div className="dialog">
+      <div className="dialog__heading">
+        <h4 className="dialog__title">{title}</h4>
+        <button className="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
+      </div>
+      <div className="dialog__content">{children}</div>
+    </div>
+  );
+};
+
+Dialog.propTypes = {
+  isOpen: PropTypes.bool,
+};
 
 export default Dialog;
