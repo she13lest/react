e63c4e864348258ec8e11f5dfce750931659c06f
Anna <she13lest@gmail.com>
2022-08-19 16:51:16 +0200

    new

--- lesson12/toDoList/src/Task.jsx
+++ lesson12/toDoList/src/Task.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 const Task = ({ done, text }) => {
-  const listItemClasses = `list-item ${done ? "list-item_done" : ""}`;
-
   return (
-    <li className={listItemClasses}>
+    <li
+      className={classNames("list-item", {
+        "list-item_done": done,
+      })}
+    >
       <input
         type="checkbox"
         className="list-item__checkbox"
         defaultChecked={done}
       />
-      <span className="list-item_text">{text}</span>
+      <span className="list-item__text">{text}</span>
 
       <button className="list-item__delete-btn"></button>
     </li>
