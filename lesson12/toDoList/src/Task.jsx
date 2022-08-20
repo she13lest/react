cfa054116755799bfe31e546241f81938c47876d
Anna <she13lest@gmail.com>
2022-08-20 15:50:14 +0200

    new

--- lesson12/toDoList/src/Task.jsx
+++ lesson12/toDoList/src/Task.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
-const Task = ({ done, text }) => {
+const Task = ({ id, done, text, onChange, onDelete }) => {
   return (
     <li
       className={classNames("list-item", {
@@ -12,10 +12,14 @@ const Task = ({ done, text }) => {
         type="checkbox"
         className="list-item__checkbox"
         defaultChecked={done}
+        onChange={() => onChange(id)}
       />
       <span className="list-item__text">{text}</span>
 
-      <button className="list-item__delete-btn"></button>
+      <button
+        className="list-item__delete-btn"
+        onClick={() => onDelete(id)}
+      ></button>
     </li>
   );
 };
--- lesson12/toDoList/src/Taskslist.jsx
+++ lesson12/toDoList/src/Taskslist.jsx
@@ -12,13 +12,53 @@ class Taskslist extends React.Component {
       { text: "Buy meat", done: true, id: 5 },
     ],
   };
+
+  onCreate = (text) => {
+    const { tasks } = this.state;
+    const newTask = {
+      text,
+      done: false,
+      id: Math.random(),
+    };
+
+    const updatedTasks = tasks.concat(newTask);
+
+    this.setState({
+      tasks: updatedTasks,
+    });
+  };
+
+  hanleTaskStatusChange = (id) => {
+    const updatedTasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: !task.done,
+        };
+      }
+      return task;
+    });
+    this.setState({ tasks: updatedTasks });
+  };
+
+  handleTaskDelete = (id) => {
+    const updatedTasks = this.state.tasks.filter((task) => task.id !== id);
+    this.setState({ tasks: updatedTasks });
+  };
+
   render() {
+    const sortedList = this.state.tasks.slice().sort((a, b) => a.done - b.done);
     return (
       <main className="todo-list">
-        <CreateTaskInput />
+        <CreateTaskInput onCreate={this.onCreate} />
         <ul className="list">
-          {this.state.tasks.map((task) => (
-            <Task key={task.id} {...task} />
+          {sortedList.map((task) => (
+            <Task
+              key={task.id}
+              {...task}
+              onChange={this.hanleTaskStatusChange}
+              onDelete={this.handleTaskDelete}
+            />
           ))}
         </ul>
       </main>
